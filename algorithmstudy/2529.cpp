f75baa999424be541d7eb1d12f03e0f2db875d55
Gatsjy <gkswndks123@naver.com>
2020-09-12 12:58:20 +0900

    2020-09-12, 한주안, 2529 백트래킹으로 풀기 .. 실패

--- algorithmstudy/2529.cpp
+++ algorithmstudy/2529.cpp
@@ -1,55 +1,86 @@
 #include <vector>
 #include <iostream>
 #include <queue>
+#include <algorithm>
+#include <string>
 
 using namespace std;
 
-int t;
-vector<bool> ch;
-vector<vector<int>> v;
+#define INF 2147000000;
 
-int bfs(vector<vector<int>> v) {
-	queue<int> q;
-	q.push(1);
-	int ans = -1;
-	while (!q.empty()) {
-		auto cur = q.front();
-		q.pop();
-		if (!ch[cur]) {
-			ch[cur] = true;
-			ans++;
-			for (int i = 0; i < v[cur].size(); i++) {
-				int togo = v[cur][i];
-				if (!ch[togo]) {
-					q.push(togo);
-				}
+vector<int> v;
+int ch[10] = { 0, };
+vector<char> c;
+int n;
+
+long Max = -INF;
+string Maxs = "";
+long Min = INF;
+string Mins = "";
+
+bool IsCheck(vector<int> v, vector<char> c) {
+	for (int i = 0; i < c.size(); i++) {
+		if (c[i] == '>') {
+			if (!(v[i] > v[i + 1])) {
+				return false;
+			}
+		}
+		else if (c[i] == '<') {
+			if (!(v[i] < v[i + 1])) {
+				return false;
 			}
 		}
 	}
-	return ans;
+	return true;
 }
 
+void dfs(int idx) {
+	if (v.size() == n) {
+		if (IsCheck(v, c)) {
+			string outputs = "";
+			for (int i = 0; i < n; i++) {
+				//cout << v[i] << " ";
+				outputs += v[i] + '0';
+			}
+			if (Min > stoll(outputs)) {
+				Min = stoll(outputs);
+				Mins = outputs;
+			}
+			if (Max < stoll(outputs)) {
+				Max = stoll(outputs);
+				Maxs = outputs;
+			}
+			//cout << endl;
+		}
+	}
+	else {
+		for (int i = 0; i < 10; i++) {
+			if (ch[i] == 0) {
+				ch[i] = 1;
+				v.push_back(i);
+				dfs(i);
+				ch[i] = 0;
+				v.pop_back();
+			}
+		}
+	}
+}
 int main() {
-
+	
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	cin >> t;
-
-	while (t--) {
-		int c, m;
-		cin >> c >> m;
-		ch = vector<bool>(c + 1);
-		v = vector<vector<int>>(c + 1);
-
-		for (int i = 0; i < m; i++) {
-			int from, to;
-			cin >> from >> to;
-			v[from].push_back(to);
-			v[to].push_back(from);
-		}
-
-		cout << bfs(v) << endl;
+	cin >> n;
+	c = vector<char>(n);
+	for (int i = 0; i < n; i++) {
+		cin >> c[i];
 	}
+	n = n + 1;
+
+	dfs(0);
+	
+	cout << Maxs << endl;
+	cout << Mins;
+	return 0;
 }
--- algorithmstudy/9372.cpp
+++ algorithmstudy/9372.cpp
@@ -1,55 +1,59 @@
-//#include <vector>
-//#include <iostream>
-//#include <queue>
-//
-//using namespace std;
-//
-//int t;
-//vector<bool> ch;
-//vector<vector<int>> v;
-//
-//int bfs(vector<vector<int>> v) {
-//	queue<int> q;
-//	q.push(1);
-//	int ans = -1;
-//	while (!q.empty()) {
-//		auto cur = q.front();
-//		q.pop();
-//		if (!ch[cur]) {
-//			ch[cur] = true;
-//			ans++;
-//			for (int i = 0; i < v[cur].size(); i++) {
-//				int togo = v[cur][i];
-//				if (!ch[togo]) {
-//					q.push(togo);
-//				}
-//			}
-//		}
-//	}
-//	return ans;
-//}
-//
-//int main() {
-//
-//	ios_base::sync_with_stdio(false);
-//	cin.tie(NULL);
-//	cout.tie(NULL);
-//	;
-//	cin >> t;
-//
-//	while (t--) {
-//		int c, m;
-//		cin >> c >> m;
-//		ch = vector<bool>(c + 1);
-//		v = vector<vector<int>>(c + 1);
-//
-//		for (int i = 0; i < m; i++) {
-//			int from, to;
-//			cin >> from >> to;
-//			v[from].push_back(to);
-//			v[to].push_back(from);
-//		}
-//		
-//		cout << bfs(v) << endl;
-//	}
-//}
+#include <vector>
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+namespace BOJ_9372 {
+
+	int t;
+	vector<bool> ch;
+	vector<vector<int>> v;
+
+	int bfs(vector<vector<int>> v) {
+		queue<int> q;
+		q.push(1);
+		int ans = -1;
+		while (!q.empty()) {
+			auto cur = q.front();
+			q.pop();
+			if (!ch[cur]) {
+				ch[cur] = true;
+				ans++;
+				for (int i = 0; i < v[cur].size(); i++) {
+					int togo = v[cur][i];
+					if (!ch[togo]) {
+						q.push(togo);
+					}
+				}
+			}
+		}
+		return ans;
+	}
+
+	int main() {
+
+		ios_base::sync_with_stdio(false);
+		cin.tie(NULL);
+		cout.tie(NULL);
+		cin >> t;
+
+		while (t--) {
+			int c, m;
+			cin >> c >> m;
+			ch = vector<bool>(c + 1);
+			v = vector<vector<int>>(c + 1);
+
+			for (int i = 0; i < m; i++) {
+				int from, to;
+				cin >> from >> to;
+				v[from].push_back(to);
+				v[to].push_back(from);
+			}
+
+			cout << bfs(v) << endl;
+		}
+
+		return 0;
+	}
+}
