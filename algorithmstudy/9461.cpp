0227969ada16892acfc5a8f7111d288c737378ca
Gatsjy <gkswndks123@naver.com>
2020-09-12 11:55:16 +0900

    초기 커밋.

---
+++ algorithmstudy/9461.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+int main() {
+    long long dp[101] = { 0,1,1,1,2,2,3,4,5,7,9, };
+
+    for (int i = 11; i <= 100; i++) {
+        dp[i] = dp[i - 5] + dp[i - 1];
+    }
+
+    int cnt;
+    scanf("%d", &cnt);
+
+    for (int i = 0; i < cnt; i++) {
+        int n;
+        scanf("%d", &n);
+        printf("%lld\n", dp[n]);
+    }
+
+}
+​
