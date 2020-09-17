e75462b4f405e3c3d1d58f671e1b7714f2626baf
Gatsjy <gkswndks123@naver.com>
2020-09-17 20:04:05 +0900

    2020-09-17, 한주안, 두번째 도전 (성공)

--- algorithmstudy/큰수만들기.cpp
+++ algorithmstudy/큰수만들기.cpp
@@ -1,48 +1,30 @@
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <iostream>
-
-#define INF 2147000000
 
 using namespace std;
 
-vector<int> a;
-vector<int> ch;
-vector<int> v;
-int n = 0;
-long long maxv = -INF;
-void dfs(int start, int k) {
-	if (v.size() == k) {
-		long long tmp = 0;
-		for (int i = 0; i < v.size(); i++) {
-			tmp = tmp * 10 + v[i];
-			//cout << v[i] << " ";
-		}
-		//cout << endl;
-		maxv = max(maxv, tmp);
-	}
-	else {
-		for (int i = start; i < a.size(); i++) {
-			if (ch[i] == 0) {
-				ch[i] = 1;
-				v.push_back(a[i]);
-				dfs(i + 1, k);
-				ch[i] = 0;
-				v.pop_back();
+string solution(string number, int k) {
+	string answer = "";
+
+	// 현재 위치를 표현할 idx
+	int cur = 0;
+	// 정답의 길이
+	int n = number.size() - k;
+	for (int i = 0; i < n; i++) {
+		int max_idx = cur;
+		for (int j = cur + 1; j < number.size(); j++) {
+			if (number.size() - j < n - answer.size()) break;
+			// idx 가 0 1 2 3 4 5 6 이면
+			// 그 뒤에 들어갈 수 있는 수의 사이즈는
+			// 차례로 6 5 4 3 2 1 0 일 것이다
+			// ex) 34** -> 별표 첫번째 자리는 뒤에 한개, 별표 두번째 자리는 뒤에 0개
+			// 가장 최소한의 값까지 포문을 돌리고 원하는 값이 없으면 앞에서 최대값을 더해준다.
+			if (number[j] > number[max_idx] && number.size() - j >= n - answer.size()) {
+				max_idx = j;
 			}
 		}
+		answer.push_back(number[max_idx]);
+		cur = ++max_idx;
 	}
-}
-
-string solution(string number, int k) {
-	for (int i = 0; i < number.size(); i++) {
-		a.push_back(number[i] - '0');
-	}
-	n = a.size();
-	ch = vector<int>(n + 1);
-	dfs(0, n - k);
-
-	string answer = to_string(maxv);
 	return answer;
 }
