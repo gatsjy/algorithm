4608a21ea534e35c0fe5f1c76da7e58b91b18982
Gatsjy <Gatsjy@DESKTOP-4TT9522>
2020-09-20 07:43:23 +0900

    2020-09-20, 한주안, 정수삼각형

--- algorithmstudy/2번문항.cpp
+++ algorithmstudy/2번문항.cpp
@@ -8,20 +8,33 @@ int dx[4] = { 1,0,-1,0 };
 int dy[4] = { 0,1,0,-1 };
 
 bool bfs(vector<string> drum, vector<vector<bool>> ch, int startX, int startY) {
+	
+	// 1. 마지막 탈출구를 구현하다. (한개의 행을 생성해서 이를 표현)
+	string lastRow = "";
+	for (int i = 0; i < drum[0].size(); i++) {
+		lastRow += "E";
+	}
+	drum.push_back(lastRow);
 	queue<pair<int, int>> q;
 	q.push({startX, startY});
 	ch[startX][startY] = true;
+	int restrict = 1;
 	while (!q.empty()) {
 		auto cur = q.front();
 		q.pop();
 		int xx = cur.first;
 		int yy = cur.second;
 		// # 은 아래로 한칸을 의미합니다. (-1,0)
+		if (restrict == 0) {
+			break;
+		}
+
 		if (drum[cur.first][cur.second] == '#') {
 			xx += 1;
 		}
 		// * 은 처음만나면 아래로 한칸, 한번더 만나면 거기서 멈춰 버립니다. (-1,0)
 		else if (drum[cur.first][cur.second] == '*') {
+			restrict--;
 			xx += 1;
 		}
 		// > 은 오른쪽으로 움직이는 것을 의미합니다. (0,1)
@@ -38,19 +51,19 @@ bool bfs(vector<string> drum, vector<vector<bool>> ch, int startX, int startY) {
 		// 그리고 첫번째 줄에서 drum.size() 만큼 for문을 어떻게 돌려 낼 수 있을까?
 		// 위에서 제기된 세가지 의문을 풀어야만 이 문제를 해결 할 수 있다.
 		// 아니 왜 나는 자꾸 BFS에 집착하는 거지??
-		if (xx < 0 || yy < 0 || xx >= drum.size() || yy >= drum.size()) continue;
+		if (xx < 0 || yy < 0 || xx >= drum.size()+1 || yy >= drum.size()) continue;
 		if (ch[xx][yy]) continue;
+		if (drum[xx][yy] == 'E') return true;
 		q.push({ xx,yy });
 		ch[xx][yy] = true;
 	}
-
-	return true;
+	return false;
 }
 
 int solution(vector<string> drum) {
 
 	int answer = 0;
-	vector<vector<bool>> ch = vector<vector<bool>>(drum.size(), vector<bool>(drum.size(), 0));
+	vector<vector<bool>> ch = vector<vector<bool>>(drum.size(), vector<bool>(drum.size()+1, 0));
 	for (int i = 0; i < drum.size(); i++) {
 		if (bfs(drum, ch, 0, i)) {
 			answer++;
