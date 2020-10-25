c836a074962bf3339c876d06f9fab9093b4128a6
Gatsjy <gkswndks123@naver.com>
2020-10-25 21:05:03 +0900

    2020-10-25, 한주안, 백준 10951

---
+++ Bakjoon/10951.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+namespace _10951 {
+	int main() {
+		ios_base::sync_with_stdio(false);
+		cin.tie(NULL);
+		cout.tie(NULL);
+		int a, b;
+		while (cin >> a >> b) {
+			cout << a + b << endl;
+		}
+		return 0;
+	}
+}
