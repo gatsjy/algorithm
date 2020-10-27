7061546b66eda25a9e61953ab1dd4e1603a453c0
Gatsjy <gkswndks123@naver.com>
2020-10-27 23:50:00 +0900

    2020-10-27, 한주안, 스터디 후 알고리즘

---
+++ Bakjoon/11866.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int n, k;
+	cin >> n >> k;
+	vector<int> a(n);
+	for (int i = 1; i <= n; i++) {
+		a[i-1] = i;
+	}
+	cout << "<";
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < k - 1; j++) {
+			a.push_back(a[0]);
+			a.erase(a.begin());
+		}
+		if (i == n - 1) {
+			cout << a[0];
+		}
+		else {
+			cout << a[0] << ", ";
+		}
+		a.erase(a.begin());
+	}
+	cout << ">";
+	return 0;
+}
