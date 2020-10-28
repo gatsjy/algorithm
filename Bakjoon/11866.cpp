3c18068926d4f85ea94ca984e3bbe2c500a18e08
Gatsjy <gkswndks123@naver.com>
2020-10-28 22:14:10 +0900

    2020-10-28, 한주안, 알고리즘 공부 2003

--- Bakjoon/11866.cpp
+++ Bakjoon/11866.cpp
@@ -3,27 +3,29 @@
 
 using namespace std;
 
-int main() {
-	int n, k;
-	cin >> n >> k;
-	vector<int> a(n);
-	for (int i = 1; i <= n; i++) {
-		a[i-1] = i;
-	}
-	cout << "<";
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < k - 1; j++) {
-			a.push_back(a[0]);
+namespace _11866{
+	int main() {
+		int n, k;
+		cin >> n >> k;
+		vector<int> a(n);
+		for (int i = 1; i <= n; i++) {
+			a[i - 1] = i;
+		}
+		cout << "<";
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < k - 1; j++) {
+				a.push_back(a[0]);
+				a.erase(a.begin());
+			}
+			if (i == n - 1) {
+				cout << a[0];
+			}
+			else {
+				cout << a[0] << ", ";
+			}
 			a.erase(a.begin());
 		}
-		if (i == n - 1) {
-			cout << a[0];
-		}
-		else {
-			cout << a[0] << ", ";
-		}
-		a.erase(a.begin());
+		cout << ">";
+		return 0;
 	}
-	cout << ">";
-	return 0;
 }
---
+++ Bakjoon/2003.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+	int n, m;
+	cin >> n >> m;
+	vector<int> a(n);
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	int cnt = 0;
+	for (int i = 0; i < n; i++) {
+		int sum = 0;
+		for (int j = i; j < n; j++) {
+			sum += a[j];
+			if (sum > m) {
+				break;
+			}
+			else {
+				if (sum == m) {
+					cnt++;
+				}
+			}
+		}
+	}
+	cout << cnt;
+}
