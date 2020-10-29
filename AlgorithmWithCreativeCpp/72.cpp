2b2a34fc6c5d68fb6b14aa1f8018a1af3f782b66
Gatsjy <gkswndks123@naver.com>
2020-10-29 23:17:51 +0900

    2020-10-29, 한주안, 알고리즘 공부

--- AlgorithmWithCreativeCpp/72.cpp
+++ AlgorithmWithCreativeCpp/72.cpp
@@ -2,24 +2,26 @@
 
 using namespace std;
 
-int main() {
-	queue<int> q;
-	int n, k;
-	cin >> n >> k;
-	for (int i = 1; i <= n; i++) {
-		q.push(i);
-	}
-
-	int cnt = 0;
-	while (!q.empty()) {
-		for (int i = 1; i < k; i++) {
-			q.push(q.front());
-			q.pop();
+namespace _72 {
+	int main() {
+		queue<int> q;
+		int n, k;
+		cin >> n >> k;
+		for (int i = 1; i <= n; i++) {
+			q.push(i);
 		}
-		q.pop();
-		if (q.size() == 1) {
-			cout << q.front();
+
+		int cnt = 0;
+		while (!q.empty()) {
+			for (int i = 1; i < k; i++) {
+				q.push(q.front());
+				q.pop();
+			}
 			q.pop();
+			if (q.size() == 1) {
+				cout << q.front();
+				q.pop();
+			}
 		}
 	}
 }
