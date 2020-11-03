04b87fd674e6838c8eca98e8cf1e3f89627b0684
Gatsjy <Gatsjy@DESKTOP-4TT9522>
2020-11-03 17:34:31 +0900

    2020-11-03, 한주안

--- Bakjoon/11098.cpp
+++ Bakjoon/11098.cpp
@@ -1,34 +1,33 @@
 #include <bits/stdc++.h>
 
-using namespace std;
-
 namespace _11098 {
-	struct player {
-		string name;
-		long price;
-		player(string s, long p) {
-			name = s;
-			price = p;
-		}
-		bool operator<(const player& b) const {
-			return price < b.price;
-		}
-	};
-	int main() {
-		int c;
-		cin >> c;
-		while (c--) {
-			int n;
-			cin >> n;
-			priority_queue<player> pq;
-			long price;
-			string name;
-			for (int i = 0; i < n; i++) {
-				cin >> price >> name;
-				pq.push(player(name, price));
-			}
-			cout << pq.top().name << endl;
-		}
-		return 0;
+	using namespace std;
+
+struct player {
+	string name;
+	long price;
+	player(string s, long p) {
+		name = s;
+		price = p;
 	}
+	bool operator<(const player& b) const {
+		return price < b.price;
+	}
+};
+int main() {
+	int c;
+	cin >> c;
+	while (c--) {
+		int n;
+		cin >> n;
+		priority_queue<player> pq;
+		long price;
+		string name;
+		for (int i = 0; i < n; i++) {
+			cin >> price >> name;
+			pq.push(player(name, price));
+		}
+		cout << pq.top().name << endl;
+	}
+	return 0;
 }
