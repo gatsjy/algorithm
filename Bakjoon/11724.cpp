66b96b0d5e305ef5ac9461b4bc77311e6c056157
Gatsjy <gkswndks123@naver.com>
2020-11-13 23:54:33 +0900

    2020-11-13, 한주안, 알고리즘 관련 문제 풀이 (백준 1208 부분수열의 합)

--- Bakjoon/11724.cpp
+++ Bakjoon/11724.cpp
@@ -2,39 +2,41 @@
 #include <iostream>
 
 using namespace std;
-
-int unf[1001];
-int cnt = 0;
-int Find(int v) {
-	if (unf[v] == v)return v;
-	else unf[v] = Find(unf[v]);
-}
-
-void Union(int a, int b) {
-	a = Find(a);
-	b = Find(b);
-	if (a != b) {
-		unf[b] = a;
-		cnt--;
+namespace _11724 {
+	int unf[1001];
+	int cnt = 0;
+	int Find(int v) {
+		if (unf[v] == v)return v;
+		else unf[v] = Find(unf[v]);
 	}
-}
 
-int main() {
-	int n, m, a, b;
-	cin >> n >> m;
-
-	for (int i = 1; i <= n; i++) {
-		unf[i] = i;
-	}
-	cnt = n;
-	for (int i = 1; i <= m; i++) {
-		cin >> a >> b;
-		if (Find(a) != Find(b)) {
-			Union(a, b);
+	void Union(int a, int b) {
+		a = Find(a);
+		b = Find(b);
+		if (a != b) {
+			unf[b] = a;
+			cnt--;
 		}
 	}
 
-	cout << cnt;
+	int main() {
+		int n, m, a, b;
+		cin >> n >> m;
 
-	return 0;
+		for (int i = 1; i <= n; i++) {
+			unf[i] = i;
+		}
+		cnt = n;
+		for (int i = 1; i <= m; i++) {
+			cin >> a >> b;
+			if (Find(a) != Find(b)) {
+				Union(a, b);
+			}
+		}
+
+		cout << cnt;
+
+		return 0;
+	}
 }
+
---
+++ Bakjoon/1208.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int n, s;
+vector<int> v;
+map<int, int> m;
+int half = 0;
+long long ans = 0;
+void dfsLeft(int idx, int sum) {
+	if (idx == half) {
+		m[sum]++;
+		return;
+	}
+	dfsLeft(idx + 1, sum + v[idx]);
+	dfsLeft(idx + 1, sum);
+}
+
+void dfsRight(int idx, int sum) {
+	if (idx == n) {
+		ans += m[s - sum];
+		return;
+	}
+	dfsRight(idx + 1, sum + v[idx]);
+	dfsRight(idx + 1, sum);
+}
+
+int main() {
+	cin >> n >> s;
+	v.resize(n);
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
+	half = n / 2;
+	dfsLeft(0, 0);
+	dfsRight(half, 0);
+	if (s == 0) ans--;
+	cout << ans;
+	return 0;
+}
