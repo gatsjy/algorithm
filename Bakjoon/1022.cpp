eeaf51fa36f4d4d3da566676b3a310f151f92bd0
Gatsjy <Gatsjy@DESKTOP-4TT9522>
2020-11-19 20:09:32 +0900

    2020-11-19, 한주안, 커멧

--- Bakjoon/1022.cpp
+++ Bakjoon/1022.cpp
@@ -3,78 +3,81 @@
 // 2020-11-16, 한주안, 느낀점.. 
 // 좌표 이용법에 대해서 다시 공부해야 할 듯...
 using namespace std;
-int m[50][5] = { 0, };
-int r1, c1, r2, c2;
 
-int calLength(int n) {
-	return n > 0 ? calLength(n / 10) + 1 : 1;
-}
+namespace _1022 {
+	int m[50][5] = { 0, };
+	int r1, c1, r2, c2;
 
-int main() {
-	cin >> r1 >> c1 >> r2 >> c2;
-	
-	int MAX = max(max(abs(r1), abs(c1)), max(abs(r2), abs(c2)));
-	// 하나씩 증가해 갈 수
-	int val = 1; 
-	if (r1 <= 0 && c1 <= 0 && r2 >= 0 && c2 >= 0) {
-		m[0 - r1][0 - c1] = val;
-	}
-	int max_val = -1;
-
-	int cnt = 0; // 몇번째 카운트인지 측정
-	int y = 0, x = 0;
-	for (int i = 1; i <= MAX + 1; i++) {
-		for (int j = 1; j <= 1 + cnt * 2; j++) {
-			val++;
-			x++;
-			if (y >= r1 && x >= c1 && y <= r2 && x <= c2) {
-				m[y - r1][x - c1] = val;
-				if (max_val < val) max_val = val;
-			}
-		}
-		for (int j = 1; j <= 1 + cnt * 2; j++) {
-			val++;
-			y--;
-			if (y >= r1 && x >= c1 && y <= r2 && x <= c2) {
-				m[y - r1][x - c1] = val;
-				if (max_val < val) max_val = val;
-			}
-		}
-		for (int j = 1; j <= 2 + cnt * 2; j++) {
-			val++;
-			x--;
-			if (y >= r1 && x >= c1 && y <= r2 && x <= c2) {
-				m[y - r1][x - c1] = val;
-				if (max_val < val) max_val = val;
-			}
-		}
-		for (int j = 1; j <= 2 + cnt * 2; j++) {
-			val++;
-			y++;
-			if (y >= r1 && x >= c1 && y <= r2 && x <= c2) {
-				m[y - r1][x - c1] = val;
-				if (max_val < val) max_val = val;
-			}
-		}
-		cnt++;
+	int calLength(int n) {
+		return n > 0 ? calLength(n / 10) + 1 : 1;
 	}
 
-	int max_val_length = calLength(max_val);
+	int main() {
+		cin >> r1 >> c1 >> r2 >> c2;
 
-	int h = abs(r2-r1);
-	int w = abs(c2-c1);
+		int MAX = max(max(abs(r1), abs(c1)), max(abs(r2), abs(c2)));
+		// 하나씩 증가해 갈 수
+		int val = 1;
+		if (r1 <= 0 && c1 <= 0 && r2 >= 0 && c2 >= 0) {
+			m[0 - r1][0 - c1] = val;
+		}
+		int max_val = -1;
 
-	for (int i = 0; i <= h; i++) {
-		for (int j = 0; j <= w; j++) {
-			int tmpLength = calLength(m[i][j]);
-			if (max_val_length > tmpLength) {
-				for (int k = 0; k < max_val_length - tmpLength; k++) {
-					cout << " ";
+		int cnt = 0; // 몇번째 카운트인지 측정
+		int y = 0, x = 0;
+		for (int i = 1; i <= MAX + 1; i++) {
+			for (int j = 1; j <= 1 + cnt * 2; j++) {
+				val++;
+				x++;
+				if (y >= r1 && x >= c1 && y <= r2 && x <= c2) {
+					m[y - r1][x - c1] = val;
+					if (max_val < val) max_val = val;
 				}
 			}
-			cout << m[i][j]<< " ";
+			for (int j = 1; j <= 1 + cnt * 2; j++) {
+				val++;
+				y--;
+				if (y >= r1 && x >= c1 && y <= r2 && x <= c2) {
+					m[y - r1][x - c1] = val;
+					if (max_val < val) max_val = val;
+				}
+			}
+			for (int j = 1; j <= 2 + cnt * 2; j++) {
+				val++;
+				x--;
+				if (y >= r1 && x >= c1 && y <= r2 && x <= c2) {
+					m[y - r1][x - c1] = val;
+					if (max_val < val) max_val = val;
+				}
+			}
+			for (int j = 1; j <= 2 + cnt * 2; j++) {
+				val++;
+				y++;
+				if (y >= r1 && x >= c1 && y <= r2 && x <= c2) {
+					m[y - r1][x - c1] = val;
+					if (max_val < val) max_val = val;
+				}
+			}
+			cnt++;
 		}
-		cout << "\n";
+
+		int max_val_length = calLength(max_val);
+
+		int h = abs(r2 - r1);
+		int w = abs(c2 - c1);
+
+		for (int i = 0; i <= h; i++) {
+			for (int j = 0; j <= w; j++) {
+				int tmpLength = calLength(m[i][j]);
+				if (max_val_length > tmpLength) {
+					for (int k = 0; k < max_val_length - tmpLength; k++) {
+						cout << " ";
+					}
+				}
+				cout << m[i][j] << " ";
+			}
+			cout << "\n";
+		}
+		return 0;
 	}
-	return 0;
 }
---
+++ Bakjoon/1600.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int dy[4] = { 1,0,-1,0 };
+int dx[4] = { 0,1,0,-1 };
+int horsey[8] = { 2,1,-1,-2,-2,-1,1,2 };
+int horsex[8] = { 1,2,2,1,-1,-2,-2,-1 };
+
+int k, w, h;
+
+int map[201][201];
+int ch[201][201];
+
+int main() {
+    cin >> k >> w >> h;
+
+    for (int i = 0; i < w; i++) {
+        for (int j = 0; j < h; j++) {
+            cin >> map[i][j];
+        }
+    }
+    queue<pair<int, int>> q;
+    q.push({ 0,0 });
+    ch[0][0] = 1;
+
+    while (!q.empty()) {
+        auto cur = q.front();
+        q.pop();
+        for (int i = 0; i < 4; i++) {
+            int xx = cur.second + dx[i];
+            int yy = cur.first + dy[i];
+            if (xx < 0 || yy < 0 || xx >= w || yy >= h) continue;
+            if (ch[yy][xx] == 1 && k > 0) {
+                for (int j = 0; j < 8; j++) {
+                    yy += horsey[j];
+                    xx += horsex[j];
+                    if (xx < 0 || yy < 0 || xx >= w || yy >= h) continue;
+                    if (map[yy][xx] == 1) continue;
+                    ch[yy][xx] = ch[cur.first][cur.second] + 1;
+                    q.push({ yy,xx });
+                    k--;
+                }
+            }
+            else {
+                continue;
+            }
+            map[yy][xx] = map[cur.first][cur.second] + 1;
+            ch[yy][xx] = 1;
+            q.push({ yy,xx });
+        }
+    }
+    return 0;
+}
