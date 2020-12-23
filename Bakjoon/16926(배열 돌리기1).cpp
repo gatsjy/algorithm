02984d529af55f0909424d7ea06a4b25f157a854
Gatsjy <gkswndks123@naver.com>
2020-12-23 23:26:59 +0900

    2020-12-23, 한주안, 배열 돌리기

--- Bakjoon/16926(배열 돌리기1).cpp
+++ Bakjoon/16926(배열 돌리기1).cpp
@@ -6,18 +6,39 @@ using namespace std;
 
 int n, m, cnt;
 int board[302][302];
-int nboard[302][302];
-int ch[302][302];
+
+// 0 : 남, 1 : 동, 2 : 북, 3 : 서
+int dx[4] = { 1,0,-1,0 };
+int dy[4] = { 0,1,0,-1 };
+bool flag = true;
+// x,y 위치, 
+void dfs(int x, int y, int dir, int next, int gSeq) {
+
+    int cur = board[x][y];
+    board[x][y] = next;
+    int nx = x + dx[dir];
+    int ny = y + dy[dir];
+
+    // 만약 방향이 '서'이고 원위치로 돌아온 경우 return
+    if ((dir == 3 && (x == gSeq && y == gSeq)) || dir >=4) {
+        return;
+    };
+
+    // 움직인다면 세로인 경우는 nx + n - gSeq 까지 이동가능, 가로는 ny + m - gSeq까지 이동가능
+    // 남, 동, 북은 위 조건식으로 잡을 수 있지만, 서에서 북에서 서로 꺾을때는 잡지 못함.
+    if (nx > (n - gSeq - 1) || ny > (m - gSeq - 1) || nx < 0 || ny < 0)
+    {
+        nx = x + dx[dir + 1];
+        ny = y + dy[dir + 1];
+        dfs(nx, ny, dir + 1, cur, gSeq);
+    }
+    else {
+        dfs(nx, ny, dir, cur, gSeq);
+    }
+}
 
 int main() {
     cin >> n >> m >> cnt;
-
-    // 1. ch 배열 할당
-    for (int i = 0; i < 302; i++) {
-        for (int j = 0; j < 302; j++) {
-            ch[i][j] = -1;
-        }
-    }
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
@@ -25,102 +46,18 @@ int main() {
         }
     }
 
-    // 1. 단계에 따라서 가장자리만 출력
-    // 2. ch배열을 만들어서 덧씌우다 보면 순서별로 가장자리를 알 수 있다.
-    for (int i = 0; i <= n / 2; i++) {
-        if (i == 0) {
-            for (int k = i; k < n; k++) {
-                for (int h = i; h < m; h++) {
-                    ch[k][h] = i;
-                }
-            }
-        }
-        else {
-            for (int k = i; k <= n - (2 * i); k++) {
-                for (int h = i; h <= m - (2 * i); h++) {
-                    ch[k][h] = i;
-                }
-            }
-        }
-    }
-
     // 이제 케이스 수 만큼 배열 회전시키기
+    // * 케이스의 그룹이 한단계 한단계 깊어질수록 시작 위치는 (1,1)씩 증가한다.
+    // ** (0,0)에서 시작하면 한바퀴를 돌고난 후에 (0,0)으로 돌아온다.
     while (cnt--) {
-        for (int i = 0; i <= n / 2; i++) {
-            
-            int counter = i;
-
-            if (i == 0) {
-                // 기준 i,i -> i+n-1, i+m-1
-                // 1. 남
-                for (int k = i; k < n; k++) {
-                    if (counter == i) {
-                        // 시작점 i,i
-                        nboard[i+k+1][i] = board[i+k][i];
-                    }
-                }
-                // 2. 동
-                for (int h = i; h < m; h++) {
-                    if (counter == i) {
-                        // 시작점 i+n,i
-                        nboard[i + n][i + h+1] = board[i + n][i + h];
-                    }
-                }
-                // 3. 북
-                for (int k = i; k < n; k++) {
-                    if (counter == i) {
-                        // 시작점 i+n,i+m
-                        nboard[i + n - k-1][i + m] = board[i + n - k][i + m];
-                    }
-                }
-                // 4. 서
-                for (int h = i; h < m; h++) {
-                    if (counter == i) {
-                        // 시작점 i, i+m
-                        nboard[i][i + m - h-1] = board[i][i + m - h];
-                    }
-                }
-            }
-            else {
-                // 기준 i,i -> n - (2 * i) , m - (2 * i)
-                // 1. 남
-                for (int k = i; k < n; k++) {
-                    if (counter == i) {
-                        // 시작점 i,i
-                        nboard[i + k + 1][i] = board[i + k][i];
-                    }
-                }
-                // 2. 동
-                for (int h = i; h < m; h++) {
-                    if (counter == i) {
-                        // 시작점 i+n - (2 * i),i
-                        nboard[i + n - (2 * i)][i + h + 1] = board[i + n - (2 * i)][i + h];
-                    }
-                }
-                // 3. 북
-                for (int k = i; k < n; k++) {
-                    if (counter == i) {
-                        // 시작점 i+n - (2 * i),i+m - (2 * i)
-                        nboard[i + n - (2 * i) - k - 1][i + m - (2 * i)] = board[i + n - (2 * i) - k][i + m - (2 * i)];
-                    }
-                }
-                // 4. 서
-                for (int h = i; h < m; h++) {
-                    if (counter == i) {
-                        // 시작점 i, i+m - (2 * i)
-                        nboard[i][i + m - (2 * i) - h - 1] = board[i][i + m - (2 * i) - h];
-                    }
-                }
-            }
-        }
-        // 이동된 배열 할당
-        for (int i = 0; i < n; i ++ ) {
-            for (int j = 0; j < m; j++) {
-                board[i][j] = nboard[i][j];
-            }
+        // 그릅 수 만큼 dfs를 돌린다.
+        for (int i = 0; i < n / 2; i++) {
+        //for (int i = 0; i <= min(n, m) / 2; i++) {
+            dfs(i, i, 0, board[i][i+1], i); // 처음 시작은 무조건 남으로 긔
         }
     }
 
+    // 정답 관련 출력
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             cout << board[i][j] << " ";
