b036d51f1a79d1071c88415df9e65767185a0b89
Gatsjy <gkswndks123@naver.com>
2021-01-08 20:33:29 +0900

    2021-01-08, 한주안, dp관련기본문제풀이

---
+++ Bakjoon/2579(계단 오르기).cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int dp[301];
+int a[301];
+int n;
+int main() {
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cin >> a[i];
+	}
+	dp[1] = a[1];
+	dp[2] = a[1]+a[2];
+	for (int i = 3; i <= n; i++) {
+		// 답답하면 내가 직접해보면 된다!!
+		dp[i] = max(a[i] + a[i - 1] + dp[i - 3], a[i] + dp[i - 2]);
+	}
+	cout << dp[n];
+	return 0;
+}
---
+++ Bakjoon/4097(수익).cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+namespace _4097 {
+	long long dp[250001];
+	long long a[250001];
+
+	int n;
+	int main() {
+		ios_base::sync_with_stdio(0);
+		cin.tie(0);
+		cout.tie(0);
+		while (1) {
+			cin >> n;
+			if (n == 0) {
+				break;
+			}
+			memset(dp, 0, sizeof(dp));
+			memset(a, 0, sizeof(a));
+
+			for (int i = 0; i < n; i++) {
+				cin >> a[i];
+			}
+			dp[0] = a[0];
+			long long res = a[0];
+			for (int i = 1; i < n; i++) {
+				if (dp[i - 1] < 0) {
+					dp[i] = a[i];
+				}
+				else {
+					dp[i] = dp[i - 1] + a[i];
+				}
+				res = max(res, dp[i]);
+			}
+			cout << res << "\n";
+		}
+		return 0;
+	}
+}
