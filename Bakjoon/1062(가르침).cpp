a89b8a084e6e616d04922d2c36e710428ecfb937
Gatsjy <gkswndks123@naver.com>
2021-01-14 20:28:54 +0900

    2021-01-14, 한주안, 1062 가르침

---
+++ Bakjoon/1062(가르침).cpp
@@ -0,0 +1,75 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int ch[26];
+vector<string> s;
+int n, k;
+vector<int> a;
+int res = -1;
+bool isKnowWard(string s) {
+	for (int i = 0; i < s.size(); i++) {
+		if (!(ch[s[i] - 'a'] == 1 || ch[s[i] - 'a'] == -1)) {
+			return false;
+		}
+	}
+	return true;
+}
+void dfs(int idx) {
+	if (a.size() == k - 5) {
+		//for (int i = 0; i < a.size(); i++) {
+		//	cout << a[i] << " ";
+		//}
+		//cout << "\n";
+		int cnt = 0;
+		for (int i = 0; i < s.size(); i++) {
+			if (isKnowWard(s[i])) cnt++;
+		}
+		res = max(res, cnt);
+	}
+	else {
+		for (int i = idx; i < 26; i++) {
+			if (ch[i] == 0) {
+				a.push_back(i);
+				ch[i] = 1;
+				dfs(i + 1);
+				a.pop_back();
+				ch[i] = 0;
+			}
+		}
+	}
+}
+int main() {
+
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	// antic 매핑
+	ch['a' - 'a'] = -1;
+	ch['n' - 'a'] = -1;
+	ch['t' - 'a'] = -1;
+	ch['i' - 'a'] = -1;
+	ch['c' - 'a'] = -1;
+
+	cin >> n >> k;
+	if (k < 5) {
+		cout << 0;
+		return 0;
+	}
+	else {
+		s.resize(n);
+		for (int i = 0; i < n; i++) {
+			cin >> s[i];
+		}
+
+		// k-5 개 순열 구하기
+		dfs(0);
+	}
+
+	cout << res;
+	return 0;
+}
--- Bakjoon/2589(보물섬).cpp
+++ Bakjoon/2589(보물섬).cpp
@@ -6,63 +6,65 @@
 
 using namespace std;
 
-int dx[4] = { -1,0,1,0 };
-int dy[4] = { 0,1,0,-1 };
+namespace _2589 {
+	int dx[4] = { -1,0,1,0 };
+	int dy[4] = { 0,1,0,-1 };
 
-char board[51][51];
-int ch[51][51];
-int n, m;
-int res = -1;
+	char board[51][51];
+	int ch[51][51];
+	int n, m;
+	int res = -1;
 
-int bfs(int fx, int fy) {
-	memset(ch, 0, sizeof(ch));
-	int result = 0;
-	queue<pair<int, int>> q;
-	q.push({ fx,fy });
-	ch[fx][fy] = 1;
-	while (!q.empty()) {
-		auto cur = q.front();
-		q.pop();
-		int x = cur.first;
-		int y = cur.second;
-		result = max(result, ch[x][y]);
-		for (int i = 0; i < 4; i++) {
-			int nx = x + dx[i];
-			int ny = y + dy[i];
-			if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-			if (board[nx][ny] == 'W' || ch[nx][ny] > 0) continue;
-			ch[nx][ny] = ch[x][y] + 1;
-			q.push({ nx,ny });
-		}
-	}
-	return result - 1;
-}
-
-int main() {
-
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
-
-	cin >> n >> m;
-
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			char input;
-			cin >> input;
-			board[i][j] = input;
-		}
-	}
-
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (board[i][j] == 'L') {
-				res = max(res, bfs(i, j));
+	int bfs(int fx, int fy) {
+		memset(ch, 0, sizeof(ch));
+		int result = 0;
+		queue<pair<int, int>> q;
+		q.push({ fx,fy });
+		ch[fx][fy] = 1;
+		while (!q.empty()) {
+			auto cur = q.front();
+			q.pop();
+			int x = cur.first;
+			int y = cur.second;
+			result = max(result, ch[x][y]);
+			for (int i = 0; i < 4; i++) {
+				int nx = x + dx[i];
+				int ny = y + dy[i];
+				if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+				if (board[nx][ny] == 'W' || ch[nx][ny] > 0) continue;
+				ch[nx][ny] = ch[x][y] + 1;
+				q.push({ nx,ny });
 			}
 		}
+		return result - 1;
 	}
 
-	cout << res;
+	int main() {
 
-	return 0;
+		ios_base::sync_with_stdio(0);
+		cin.tie(0);
+		cout.tie(0);
+
+		cin >> n >> m;
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				char input;
+				cin >> input;
+				board[i][j] = input;
+			}
+		}
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (board[i][j] == 'L') {
+					res = max(res, bfs(i, j));
+				}
+			}
+		}
+
+		cout << res;
+
+		return 0;
+	}
 }
