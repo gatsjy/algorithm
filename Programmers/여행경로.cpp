2db3ce8bef79d0685045092536fda183f4a69254
Gatsjy <gkswndks123@naver.com>
2021-01-19 20:33:32 +0900

    2021-01-19, 한주안, 여행경로 관련

--- Programmers/여행경로.cpp
+++ Programmers/여행경로.cpp
@@ -1,44 +1,43 @@
 #include <string>
 #include <vector>
-#include <map>
+#include <unordered_map>
 #include <algorithm>
 
 using namespace std;
 vector<string> answer;
 vector<string> ch;
-map<string, vector<string>> m;
+unordered_map<string, vector<string>> m;
 
-bool isFind(vector<string>& v, string s) {
-    for (int i = 0; i < v.size(); i++) {
-        if (v[i] == s) return true;
-    }
-    return false;
-}
-
-void dfs(string cur, vector<vector<string>> &tickets) {
-    answer.push_back(cur);
-    for (int i = 0; i < m[cur].size(); i++) {
-        if (!isFind(ch, m[cur][i])) {
-            ch.push_back(tickets[i][1]);
-            dfs(tickets[i][1], tickets);
-        }
-    }
-}
 
 vector<string> solution(vector<vector<string>> tickets) {
+    vector<string> answer;
+    unordered_map<string, vector<string>> m;
     for (int i = 0; i < tickets.size(); i++) {
         m[tickets[i][0]].push_back(tickets[i][1]);
     }
-    ch.push_back("ICN");
-    dfs("ICN", tickets);
 
+    vector<string> s = vector<string>{ "ICN" }; // 벡터가 stack의 역할을 해준다.
+
+    while (!s.empty()) {
+        string cur = s.back();
+        if (m.find(cur) == m.end() || m[cur].size() == 0) {
+            answer.push_back(cur);
+            s.pop_back();
+        }
+        else {
+            s.push_back(m[cur].back());
+            m[cur].pop_back();
+        }
+    }
+    reverse(answer.begin(), answer.end());
     return answer;
 }
 
 int main() {
     vector<vector<string>> tickets = { { "ICN", "JFK"},{"HND", "IAD"},{"JFK", "HND"}};
+    vector<vector<string>> tickets2 = { {"ICN", "SFO"} ,{"ICN", "ATL"},{"SFO", "ATL"},{"ATL", "ICN"},{"ATL","SFO"} };
     
-    solution(tickets);
+    solution(tickets2);
 
     return 0;
 }
