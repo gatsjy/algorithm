a6c00f151a427dc21a226dbc0c3ed5885b00d3c0
Gatsjy <gkswndks123@naver.com>
2021-01-22 20:19:02 +0900

    2021-01-22, 한주안, 숫자판 점프(2210)

---
+++ Bakjoon/2210(숫자판점프).cpp
@@ -0,0 +1,46 @@
+#include <vector>
+#include <iostream>
+#include <queue>
+#include <utility>
+#include <string>
+#include <set>
+
+using namespace std;
+
+int dx[4] = { -1,0,1,0 };
+int dy[4] = { 0,1,0,-1 };
+int board[6][6];
+set<int> ss;
+vector<char> a;
+int ch[6][6];
+void dfs(int x, int y, int level, int sum) {
+	if (level == 5) {
+		ss.insert(sum);
+		//cout << sum << "\n";
+	}
+	else {
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			if (nx < 0 || ny < 0 || nx >= 5 || ny >= 5) continue;
+			dfs(nx, ny, level + 1, sum * 10 + board[nx][ny]);
+		}
+	}
+}
+
+int main() {
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			cin >> board[i][j];
+		}
+	}
+
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			dfs(i, j, 0, board[i][j]);
+		}
+	}
+
+	cout << ss.size();
+	return 0;
+}
