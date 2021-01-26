7dcb0d9a297f1e9dd8f8ea124fbe5e11e913eee3
Gatsjy <gkswndks123@naver.com>
2021-01-26 21:02:39 +0900

    2021-01-26, 한주안, 11967(불켜기)

---
+++ Bakjoon/11967(불켜기).cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int dx[4] = { -1,0,1,0 };
+int dy[4] = { 0,1,0,-1 };
+
+int n, m;
+int x, y, a, b;
+int board[101][101];
+int ch[101][101];
+vector<pair<int, int>> um[101][101]; // 이렇게 선언하는 거에 대해서 잘 생각해보기!
+int cnt = 0;
+// 불켜진 방 세기
+int countRooms() {
+	int cnt = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (board[i][j] > 0) cnt++;
+		}
+	}
+	return cnt;
+}
+
+// 현재 자리에서 불 킬 수 있는지 확인하는 함수
+// 이전에는 갈 수 없던 방들이 불이 켜진 후엔 갈 수 있도록 코딩해야한다..
+// 이러한 방법을 구현하기 위해서 일단 불을켰는지 안켰는지 체크하고
+// 불을 한번이라도 켰으면 -> 다시 0,0부터 bfs시작
+// 안켰으면 다음 단계로 넘어간다
+bool lightUp(int x, int y) {
+	bool flag = false;
+	for (int i = 0; i < um[x][y].size(); i++) {
+		if (board[um[x][y][i].first][um[x][y][i].second] == 0) {
+			board[um[x][y][i].first][um[x][y][i].second] = 1;
+			flag = true;
+		}
+	}
+	return flag;
+}
+
+void bfs(int x, int y) {
+	queue<pair<int, int>> q;
+	memset(ch, 0, sizeof(ch));
+	q.push({ 0,0 });
+	ch[0][0] = 1;
+
+	while (!q.empty()) {
+		int x = q.front().first;
+		int y = q.front().second;
+		q.pop();
+		if (lightUp(x, y)) {
+			bfs(0, 0); // 한번 불을 켰으면 다시 bfs를 돌린다.
+		}
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
+			if (ch[nx][ny] > 0) continue;
+			if (board[nx][ny] == 0) continue;
+			ch[nx][ny] = ch[x][y] + 1;
+			q.push({ nx,ny });
+		}
+	}
+}
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> n >> m;
+	for (int i = 0; i < m; i++) {
+		cin >> x >> y >> a >> b;
+		um[--x][--y].push_back({ --a,--b });
+	}
+
+	// 일단 처음 부분 불을 킨다.
+	board[0][0] = 1;
+	map<pair<int, int>, vector<pair<int, int>>>::iterator iter;
+	bfs(0, 0);
+
+	cnt = countRooms();
+
+	cout << cnt;
+	return 0;
+}
--- Bakjoon/16929(Two Dots).cpp
+++ Bakjoon/16929(Two Dots).cpp
@@ -7,69 +7,70 @@
 #include <string>
 
 using namespace std;
-
 // https://www.acmicpc.net/problem/16929
 
-int dx[4] = { -1,0,1,0 };
-int dy[4] = { 0,1,0,-1 };
-char board[51][51];
-int ch[51][51];
-int dist[51][51]; // 이동한 거리를 체크
-int n, m;
-string s;
+namespace _16929 {
+    int dx[4] = { -1,0,1,0 };
+    int dy[4] = { 0,1,0,-1 };
+    char board[51][51];
+    int ch[51][51];
+    int dist[51][51]; // 이동한 거리를 체크
+    int n, m;
+    string s;
 
-bool dfs(int x, int y, int cnt) {
-    if (ch[x][y] > 0) {
-        if (cnt - dist[x][y] >= 4) {
-            return true;
+    bool dfs(int x, int y, int cnt) {
+        if (ch[x][y] > 0) {
+            if (cnt - dist[x][y] >= 4) {
+                return true;
+            }
+            else {
+                return false;
+            }
         }
         else {
-            return false;
-        }
-    }
-    else {
-        ch[x][y] = 1;
-        dist[x][y] = cnt;
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if (nx < 0 || ny < 0 || nx >= n || nx >= m)continue;
-            if (board[x][y] != board[nx][ny]) continue;
-            cout << nx << ", " << ny << "\n";
-            if (dfs(nx, ny, cnt + 1)) return true;
-        }
-    }
-}
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-
-    cin >> n >> m;
-
-    for (int i = 0; i < n; i++) {
-        cin >> s;
-        for (int j = 0; j < m; j++) {
-            board[i][j] = s[j];
-        }
-    }
-
-    // ** 사이클을 이룬다는 개념을 어떻게 구현할 것인가?
-    // 1. 일단 한바퀴 쭉 도는 것을 구현해보자.
-    // dfs를 진행하면서 다시 돌아왔을 경우 사이클을 이룬다고 할 수 있음
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (ch[i][j] == 0) {
-                //cout << "target " << i << " " << j << "\n";
-                if (dfs(i, j, 0)) {
-                    cout << "Yes";
-                    return 0;
-                }
+            ch[x][y] = 1;
+            dist[x][y] = cnt;
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if (nx < 0 || ny < 0 || nx >= n || nx >= m)continue;
+                if (board[x][y] != board[nx][ny]) continue;
+                cout << nx << ", " << ny << "\n";
+                if (dfs(nx, ny, cnt + 1)) return true;
             }
         }
     }
+    int main() {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
 
-    cout << "No";
+        cin >> n >> m;
 
-    return 0;
+        for (int i = 0; i < n; i++) {
+            cin >> s;
+            for (int j = 0; j < m; j++) {
+                board[i][j] = s[j];
+            }
+        }
+
+        // ** 사이클을 이룬다는 개념을 어떻게 구현할 것인가?
+        // 1. 일단 한바퀴 쭉 도는 것을 구현해보자.
+        // dfs를 진행하면서 다시 돌아왔을 경우 사이클을 이룬다고 할 수 있음
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (ch[i][j] == 0) {
+                    //cout << "target " << i << " " << j << "\n";
+                    if (dfs(i, j, 0)) {
+                        cout << "Yes";
+                        return 0;
+                    }
+                }
+            }
+        }
+
+        cout << "No";
+
+        return 0;
+    }
 }
