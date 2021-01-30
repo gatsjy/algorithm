01b0af3ff79c39b265b42c722ebdabbb23d2b1b0
Gatsjy <gkswndks123@naver.com>
2021-01-30 21:55:33 +0900

    2021-01-30, 한주안, 1차도전 로봇청소기.. 실패

--- Bakjoon/1937(욕심쟁이 판다).cpp
+++ Bakjoon/1937(욕심쟁이 판다).cpp
@@ -1,42 +1,45 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-int dx[4] = { 1,0,-1,0 };
-int dy[4] = { 0,1,0,-1 };
-int board[501][501];
-int dp[501][501];
-int n;
-int res = 1;
 
-int dfs(int x, int y) {
-	// 재귀의 기저조건
-	if (dp[x][y] != 0) return dp[x][y];
-	// 기본으로 1 채워주기 (왜냐하면 최소가 1일이기 때문에)
-	dp[x][y] = 1;
-	for (int i = 0; i < 4; i++) {
-		int nx = x + dx[i];
-		int ny = y + dy[i];
-		if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
-		if (board[x][y] < board[nx][ny]) {
-			dp[x][y] = max(dp[x][y],dfs(nx, ny) + 1);
-		}
-	}
-	return dp[x][y];
-}
-int main() {
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			cin >> board[i][j];
-		}
-	}
+namespace _1937 {
+	int dx[4] = { 1,0,-1,0 };
+	int dy[4] = { 0,1,0,-1 };
+	int board[501][501];
+	int dp[501][501];
+	int n;
+	int res = 1;
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			res = max(dfs(i, j), res);
+	int dfs(int x, int y) {
+		// 재귀의 기저조건
+		if (dp[x][y] != 0) return dp[x][y];
+		// 기본으로 1 채워주기 (왜냐하면 최소가 1일이기 때문에)
+		dp[x][y] = 1;
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
+			if (board[x][y] < board[nx][ny]) {
+				dp[x][y] = max(dp[x][y], dfs(nx, ny) + 1);
+			}
 		}
+		return dp[x][y];
 	}
+	int main() {
+		cin >> n;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				cin >> board[i][j];
+			}
+		}
 
-	cout << res;
-	return 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				res = max(dfs(i, j), res);
+			}
+		}
+
+		cout << res;
+		return 0;
+	}
 }
