8667238a7b2748ae5707b23d198e8b3b34f241e0
Gatsjy <gkswndks123@naver.com>
2021-02-22 22:04:33 +0900

    2021-02-22, 한주안, 파이썬 7일차

---
+++ AlgorithmWithCreativePython/1. 가장 큰 수(스택).py
@@ -0,0 +1,21 @@
+'''
+1. 가장 큰 수(스택)
+'''
+import sys
+from collections import deque
+sys.stdin=open("input.txt", "r")
+n,m = map(int, input().split())
+n = list(map(int, str(n)))
+
+stack=[]
+for x in n:
+    while stack and m>0 and stack[-1]<x:
+        stack.pop()
+        m-=1
+    stack.append(x)
+
+if m != 0:
+    stack = stack[:-m]
+
+res = ''.join(map(str, stack))
+print(res)
