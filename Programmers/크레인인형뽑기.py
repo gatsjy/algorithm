cbd4634d217d08dd1d9e49748a483af99288de26
Gatsjy <gkswndks123@naver.com>
2021-03-19 22:14:45 +0900

    2021-03-19, 한주안, 크레인 인형뽑기

---
+++ Programmers/크레인인형뽑기.py
@@ -0,0 +1,32 @@
+# 인형을 뽑아내는 함수
+def pickdolls(board, move, size, dolls) :
+    for i in range(size) :
+        if board[i][move - 1] != 0 :
+            dolls.append(board[i][move - 1])
+            board[i][move - 1] = 0
+            break
+
+            # 쌓인 인형에서 중복을 제거하고 그 값을 리턴해주는 함수
+            def checkBoom(dolls) :
+            res = 0
+            flag = True
+            while flag :
+                flag = False
+                for i in range(1, len(dolls)) :
+                    if dolls[i - 1] == dolls[i] :
+                        flag = True
+                        dolls.pop(i - 1)
+                        dolls.pop(i - 1)
+                        res += 2
+                        break
+                        return res
+
+                        def solution(board, moves) :
+                        answer = 0
+                        size = len(board)
+                        dolls = []
+                        for move in moves :
+pickdolls(board, move, size, dolls)
+answer += checkBoom(dolls)
+
+return answer
