a02d0521e1598cd539aaf8518863f9777908b057
Gatsjy <gkswndks123@naver.com>
2021-04-16 20:57:54 +0900

    2021-04-16, 한주안, c# 알고리즘 공부 시작

---
+++ AlgorithmCSharp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution
+{
+    public int[] solution(int[] numbers)
+    {
+        int[] answer = new int[] { };
+        SortedSet<int> tmp = new SortedSet<int>();
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            for (int j = i; j < numbers.Length; j++)
+            {
+                if (i == j) continue;
+                tmp.Add(numbers[i] + numbers[j]);
+            }
+        }
+        var tmpList = answer.ToList();
+        foreach (int i in tmp)
+        {
+            tmpList.Add(i);
+        }
+        answer = tmpList.ToArray();
+        return answer;
+    }
+}
+​
