366c3dbe9acd50a2f1efdd4463f9dec166735d8d
Gatsjy <gkswndks123@naver.com>
2022-03-05 20:56:57 +0900

    2022-03-05, 한주안, 백준 10808 긔긔

--- AlgorithmCSharp/Program.cs
+++ AlgorithmCSharp/Program.cs
@@ -2,27 +2,71 @@
 using System.Collections.Generic;
 using System.Linq;
 
-public class Solution
+class Program
 {
-    public int[] solution(int[] numbers)
+    static void Main(string[] args)
     {
-        int[] answer = new int[] { };
-        SortedSet<int> tmp = new SortedSet<int>();
-        for (int i = 0; i < numbers.Length; i++)
+        while (true)
         {
-            for (int j = i; j < numbers.Length; j++)
+            string line = Console.ReadLine();
+            if (line.Equals("end")) return;
+            bool flag = true;
+
+            int aeiouCnt = 0;
+            int vowelCnt = 0;
+            int consonantCnt = 0;
+            char prev = '-';
+
+            for (int i = 0; i < line.Length; i++)
             {
-                if (i == j) continue;
-                tmp.Add(numbers[i] + numbers[j]);
+                if(i != 0)
+                {
+                    prev = line[i - 1];
+
+                    // 같은 글자가 연속적으로 두번 오면 안되나, ee 와 oo는 허용한다.
+                    if (prev == line[i])
+                    {
+                        if(!(line[i] == 'e' || line[i] == 'o'))
+                        {
+                            flag = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (line[i] == 'a' || line[i] == 'e' || line[i] == 'i' || line[i] == 'o' || line[i] == 'u'){
+                    aeiouCnt++;
+                    vowelCnt++;
+                    consonantCnt = 0;
+                }
+                else // 자음일때
+                {
+                    vowelCnt = 0;
+                    consonantCnt++;
+                }
+
+                // 2. 모음이 3개 혹은 자음이 3개 연속으로 오면 안된다.
+                if (consonantCnt == 3 || vowelCnt == 3)
+                {
+                    flag = false;
+                    break;
+                }
+            }
+
+            // 1. 모음(a,e,i,o,u) 하나를 반드시 포함하여야 한다.
+            if(aeiouCnt == 0)
+            {
+                flag = false;
+            }
+
+            if (flag)
+            {
+                Console.WriteLine("<{0}> is acceptable.", line);
+            }
+            else
+            {
+                Console.WriteLine("<{0}> is not acceptable.", line);
             }
         }
-        var tmpList = answer.ToList();
-        foreach (int i in tmp)
-        {
-            tmpList.Add(i);
-        }
-        answer = tmpList.ToArray();
-        return answer;
     }
 }
-​
--- algorithmstudy/2번문항.cpp
+++ algorithmstudy/2번문항.cpp
@@ -7,75 +7,77 @@ using namespace std;
 int dx[4] = { 1,0,-1,0 };
 int dy[4] = { 0,1,0,-1 };
 
-bool bfs(vector<string> drum, vector<vector<bool>> ch, int startX, int startY) {
-	
-	// 1. 마지막 탈출구를 구현하다. (한개의 행을 생성해서 이를 표현)
-	string lastRow = "";
-	for (int i = 0; i < drum[0].size(); i++) {
-		lastRow += "E";
-	}
-	drum.push_back(lastRow);
-	queue<pair<int, int>> q;
-	q.push({startX, startY});
-	ch[startX][startY] = true;
-	int restrict = 1;
-	while (!q.empty()) {
-		auto cur = q.front();
-		q.pop();
-		int xx = cur.first;
-		int yy = cur.second;
-		// # 은 아래로 한칸을 의미합니다. (-1,0)
-		if (restrict == 0) {
-			break;
-		}
+namespace socar_second {
+	bool bfs(vector<string> drum, vector<vector<bool>> ch, int startX, int startY) {
 
-		if (drum[cur.first][cur.second] == '#') {
-			xx += 1;
+		// 1. 마지막 탈출구를 구현하다. (한개의 행을 생성해서 이를 표현)
+		string lastRow = "";
+		for (int i = 0; i < drum[0].size(); i++) {
+			lastRow += "E";
 		}
-		// * 은 처음만나면 아래로 한칸, 한번더 만나면 거기서 멈춰 버립니다. (-1,0)
-		else if (drum[cur.first][cur.second] == '*') {
-			restrict--;
-			xx += 1;
-		}
-		// > 은 오른쪽으로 움직이는 것을 의미합니다. (0,1)
-		else if (drum[cur.first][cur.second] == '>') {
-			yy += 1;
-		}
-		// < 은 왼쪽으로 움직이는 것을 의미합니다. (0,-1)
-		else if (drum[cur.first][cur.second] == '<') {
-			yy -= 1;
-		}
+		drum.push_back(lastRow);
+		queue<pair<int, int>> q;
+		q.push({ startX, startY });
+		ch[startX][startY] = true;
+		int restrict = 1;
+		while (!q.empty()) {
+			auto cur = q.front();
+			q.pop();
+			int xx = cur.first;
+			int yy = cur.second;
+			// # 은 아래로 한칸을 의미합니다. (-1,0)
+			if (restrict == 0) {
+				break;
+			}
 
-		// 아 근데 여기서 어떻게 잡아낼 수 있을까? 이것이 도착했다는 것을?
-		// 그리고 또한 한번 사용한 기회 (*)을 어떻게 체크해 낼 수 있을까?
-		// 그리고 첫번째 줄에서 drum.size() 만큼 for문을 어떻게 돌려 낼 수 있을까?
-		// 위에서 제기된 세가지 의문을 풀어야만 이 문제를 해결 할 수 있다.
-		// 아니 왜 나는 자꾸 BFS에 집착하는 거지??
-		if (xx < 0 || yy < 0 || xx >= drum.size()+1 || yy >= drum.size()) continue;
-		if (ch[xx][yy]) continue;
-		if (drum[xx][yy] == 'E') return true;
-		q.push({ xx,yy });
-		ch[xx][yy] = true;
-	}
-	return false;
-}
+			if (drum[cur.first][cur.second] == '#') {
+				xx += 1;
+			}
+			// * 은 처음만나면 아래로 한칸, 한번더 만나면 거기서 멈춰 버립니다. (-1,0)
+			else if (drum[cur.first][cur.second] == '*') {
+				restrict--;
+				xx += 1;
+			}
+			// > 은 오른쪽으로 움직이는 것을 의미합니다. (0,1)
+			else if (drum[cur.first][cur.second] == '>') {
+				yy += 1;
+			}
+			// < 은 왼쪽으로 움직이는 것을 의미합니다. (0,-1)
+			else if (drum[cur.first][cur.second] == '<') {
+				yy -= 1;
+			}
 
-int solution(vector<string> drum) {
-
-	int answer = 0;
-	vector<vector<bool>> ch = vector<vector<bool>>(drum.size(), vector<bool>(drum.size()+1, 0));
-	for (int i = 0; i < drum.size(); i++) {
-		if (bfs(drum, ch, 0, i)) {
-			answer++;
+			// 아 근데 여기서 어떻게 잡아낼 수 있을까? 이것이 도착했다는 것을?
+			// 그리고 또한 한번 사용한 기회 (*)을 어떻게 체크해 낼 수 있을까?
+			// 그리고 첫번째 줄에서 drum.size() 만큼 for문을 어떻게 돌려 낼 수 있을까?
+			// 위에서 제기된 세가지 의문을 풀어야만 이 문제를 해결 할 수 있다.
+			// 아니 왜 나는 자꾸 BFS에 집착하는 거지??
+			if (xx < 0 || yy < 0 || xx >= drum.size() + 1 || yy >= drum.size()) continue;
+			if (ch[xx][yy]) continue;
+			if (drum[xx][yy] == 'E') return true;
+			q.push({ xx,yy });
+			ch[xx][yy] = true;
 		}
+		return false;
 	}
 
-	return answer;
-}
+	int solution(vector<string> drum) {
 
-int main() {
-	vector<string> drum = { "######", ">#*###", "####*#","#<#>>#",">#*#*<", "######" };
-	solution(drum);
+		int answer = 0;
+		vector<vector<bool>> ch = vector<vector<bool>>(drum.size(), vector<bool>(drum.size() + 1, 0));
+		for (int i = 0; i < drum.size(); i++) {
+			if (bfs(drum, ch, 0, i)) {
+				answer++;
+			}
+		}
 
-	return 0;
+		return answer;
+	}
+
+	int main() {
+		vector<string> drum = { "######", ">#*###", "####*#","#<#>>#",">#*#*<", "######" };
+		solution(drum);
+
+		return 0;
+	}
 }
