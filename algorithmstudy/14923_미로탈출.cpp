0288ec310c3c80e8972d986396b2dde44c00d9a0
Gatsjy <gkswndks123@naver.com>
2022-03-25 23:39:30 +0900

    2022-03-25, 한주안, 2583 영역구하기 dfs +=

--- algorithmstudy/14923_미로탈출.cpp
+++ algorithmstudy/14923_미로탈출.cpp
@@ -1,70 +1,71 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-int dy[4] = { -1,0,1,0 };
-int dx[4] = { 0,1,0,-1 };
-
-struct c {
-	int x;
-	int y;
-	int magic;
-};
-int visited[1001][1001][2], a[1001][1001], res = 2147000000, n,m, sy,sx,ey,ex,y,x;
-
-int bfs(int y, int x) {
-	queue<c> q;
-	q.push({ y,x,0 });
-	visited[y][x][0] = 1;
-	while (!q.empty()) {
-		int y = q.front().y;
-		int x = q.front().x;
-		int magic = q.front().magic;
-		if (y == ey && x == ex) {
-			return visited[y][x][magic];
-		}
-		q.pop();
-		for (int i = 0; i < 4; i++) {
-			int ny = y + dy[i];
-			int nx = x + dx[i];
-			if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
-			if (a[ny][nx] == 0 ) {
-				if (visited[ny][nx][magic] == 0) {
-					visited[ny][nx][magic] = visited[y][x][magic] + 1;
-					q.push({ ny,nx,magic });
-				}
-			}
-			if (a[ny][nx] == 1) {
-				if (magic == 0)
-				{
-					visited[ny][nx][1] = visited[y][x][magic] + 1;
-					q.push({ ny,nx,1 });
-				}
-
-			}
-		}
-	}
-}
-int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	cin >> n >> m;
-
-	cin >> sx >> sy;
-	cin >> ex >> ey;
-	sy = sy - 1; sx = sx - 1; ey = ey - 1; ex = ex - 1;
-
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < m; j++) {
-			cin >> a[i][j];
-		}
-	}
-
-	int res = bfs(sy, sx);
-
-	cout << res;
-	return 0;
-}
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//namespace πÃ∑Œ≈ª√‚_14923 {
+//	int dy[4] = { -1,0,1,0 };
+//	int dx[4] = { 0,1,0,-1 };
+//
+//	struct c {
+//		int x;
+//		int y;
+//		int magic;
+//	};
+//	int visited[1001][1001][2], a[1001][1001], res = 2147000000, n, m, sy, sx, ey, ex, y, x;
+//
+//	int bfs(int y, int x) {
+//		queue<c> q;
+//		q.push({ y,x,0 });
+//		visited[y][x][0] = 1;
+//		while (!q.empty()) {
+//			int y = q.front().y;
+//			int x = q.front().x;
+//			int magic = q.front().magic;
+//			if (y == ey && x == ex) {
+//				return visited[y][x][magic];
+//			}
+//			q.pop();
+//			for (int i = 0; i < 4; i++) {
+//				int ny = y + dy[i];
+//				int nx = x + dx[i];
+//				if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+//				if (a[ny][nx] == 0) {
+//					if (visited[ny][nx][magic] == 0) {
+//						visited[ny][nx][magic] = visited[y][x][magic] + 1;
+//						q.push({ ny,nx,magic });
+//					}
+//				}
+//				if (a[ny][nx] == 1) {
+//					if (magic == 0)
+//					{
+//						visited[ny][nx][1] = visited[y][x][magic] + 1;
+//						q.push({ ny,nx,1 });
+//					}
+//
+//				}
+//			}
+//		}
+//	}
+//	int main() {
+//		ios_base::sync_with_stdio(0);
+//		cin.tie(NULL);
+//		cout.tie(NULL);
+//
+//		cin >> n >> m;
+//
+//		cin >> sx >> sy;
+//		cin >> ex >> ey;
+//		sy = sy - 1; sx = sx - 1; ey = ey - 1; ex = ex - 1;
+//
+//		for (int i = 0; i < n; i++)
+//		{
+//			for (int j = 0; j < m; j++) {
+//				cin >> a[i][j];
+//			}
+//		}
+//
+//		int res = bfs(sy, sx);
+//
+//		cout << res;
+//		return 0;
+//	}
+//}
---
+++ algorithmstudy/2583_영역구하기.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int dy[4] = { -1,0,1,0 };
+int dx[4] = { 0,1,0,-1 };
+int _map[104][104], m, n, k;
+int x11, x22, y11, y22;
+vector<int> res;
+int dfs(int y, int x) {
+	_map[y][x] = 1;
+	int res = 1;
+	for (int i = 0; i < 4; i++) {
+		int ny = y + dy[i];
+		int nx = x + dx[i];
+		if (ny < 0 || nx < 0 || ny >= m || nx >= n || _map[ny][nx] == 1)continue;
+		res += dfs(ny, nx);
+	}
+	return res;
+}
+int main() {
+	
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> m >> n >> k;
+
+	// °ª ¸ÅÇÎ
+	for (int i = 0; i < k; i++) {
+		cin >> x11 >> y11 >> x22 >> y22;
+		for (int x = x11; x < x22; x++) {
+			for (int y = y11; y < y22; y++) {
+				_map[y][x] = 1;
+			}
+		}
+	}
+
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			if (_map[i][j] != 1) {
+				res.push_back(dfs(i, j));
+			}
+		}
+	}
+
+	sort(res.begin(), res.end());
+	cout << res.size() << "\n";
+	for (int _res : res) cout << _res << " ";
+	return 0;
+}
