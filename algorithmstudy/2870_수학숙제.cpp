cbced7c9e84d7c57d8b11c325b78e0a65e7f24e4
Gatsjy <gkswndks123@naver.com>
2022-03-28 22:27:19 +0900

    2022-03-28, 한주안, 수학숙제

--- algorithmstudy/2870_수학숙제.cpp
+++ algorithmstudy/2870_수학숙제.cpp
@@ -2,42 +2,46 @@
 
 using namespace std;
 
-vector <long> res;
-
+vector<string> res;
+string input;
 int n;
 string s;
+void parseInt() {
+	if (input.size()) {
+		while (true) {
+			if (input.size() && input.front() == '0') input.erase(input.begin());
+			else break;
+		}
+		if (input.size() == 0)input = "0";
+		res.push_back(input);
+		input = "";
+	}
+}
 
-void parseInt(string s) {
-	string input;
-	for (int i = 0; i < s.size(); i++) {
-		if (s[i] >= '0' && s[i] <= '9') {
-			input += s[i];
-		}
-		else {
-			if (input != "") {
-				res.push_back(stol(input));
-			}
-			input = "";
-		}
-	}
-	if (input != "") {
-		res.push_back(stol(input));
-	}
+bool cmp(string a, string b) {
+	if (a.size() == b.size()) return a < b;
+	return a.size() < b.size();
 }
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 	cin >> n;
+	
 	for (int i = 0; i < n; i++) {
 		cin >> s;
-		parseInt(s);
+		input = "";
+		for (int j = 0; j < s.size(); j++) {
+			if (s[j] < 58) input += s[j];
+			else {
+				parseInt();
+			}
+		}
+		parseInt();
 	}
 
-	sort(res.begin(), res.end());
+	sort(res.begin(), res.end(), cmp);
 
-	for (long _res : res) {
-		cout << _res << "\n";
-	}
+	for (string s : res) cout << s << " ";
 	return 0;
 }
