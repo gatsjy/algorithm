14ccbf6ecd82a86187647ce2d1feda194493db93
Gatsjy <gkswndks123@naver.com>
2022-09-07 23:42:57 +0900

    20220907, 한주안, rot13 풀이

--- Bakjoon/1159(농구경기).cpp
+++ Bakjoon/1159(농구경기).cpp
@@ -2,32 +2,35 @@
 
 using namespace std;
 
-vector<string> p;
-int c[50];
-int n;
-int main() {
-	cin >> n;
+namespace _1159 {
+	vector<string> p;
+	int c[50];
+	int n;
+	int main() {
+		cin >> n;
 
-	for (int i = 0; i < n; i++) {
-		string input;
-		cin >> input;
-		c[input[0] - 'a']++;
-	}
-
-	string s;
-	int cnt = 0;
-	for (int i = 0; i < 30; i++) {
-		if (c[i] >= 5) {
-			s += (i + 'a');
-			cnt++;
+		for (int i = 0; i < n; i++) {
+			string input;
+			cin >> input;
+			c[input[0] - 'a']++;
 		}
-	}
 
-	if (cnt == 0) {
-		cout << "PREDAJA";
+		string s;
+		int cnt = 0;
+		for (int i = 0; i < 30; i++) {
+			if (c[i] >= 5) {
+				s += (i + 'a');
+				cnt++;
+			}
+		}
+
+		if (cnt == 0) {
+			cout << "PREDAJA";
+		}
+		else {
+			cout << s;
+		}
+		return 0;
 	}
-	else {
-		cout << s;
-	}
-	return 0;
 }
+
---
+++ Bakjoon/11655(ROT13).cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string s,res;
+int main() {
+	getline(cin, s);
+	for (int i = 0; i < s.size(); i++) {
+		// 회전도 구현해줘야 한다.
+		// 'A' = 65 'Z' = 90
+		// 'a' = 97 'z' = 122
+
+		// 소문자 부분
+		if (s[i] >= 'a' && s[i] <= 'z') {
+			if (s[i] + 13 > 122) {
+				res += s[i] - 13;
+			}
+			else {
+				res += s[i] + 13;
+			}
+		}
+		// 대문자 부분
+		else if (s[i] >= 'A' && s[i] <= 'Z') {
+			if (s[i] + 13 > 90) {
+				res += s[i] - 13;
+			}
+			else {
+				res += s[i] + 13;
+			}
+		}
+		else {
+			res += s[i];
+		}
+	}
+
+	cout << res;
+	
+	return 0;
+}
