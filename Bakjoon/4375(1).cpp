326fc21f61c5c2580dc3691f112b166a3951e4b1
Gatsjy <gkswndks123@naver.com>
2022-09-18 21:47:53 +0900

    2022-09-18, 한주안, 1

--- Bakjoon/4375(1).cpp
+++ Bakjoon/4375(1).cpp
@@ -2,20 +2,23 @@
 
 using namespace std;
 
-namespace _4375 {
-	int n;
-	int main() {
-		while (cin >> n) {
-			int num = 0;
-			for (int i = 1; i <= n; i++) {
-				num = num * 10 + 1;
-				num %= n;
-				if (num == 0) {
-					printf("%d\n", i);
-					break;
-				}
+int n;
+int main() {
+
+	while (scanf("%d",&n) != EOF) {
+		int cnt = 1, res = 1; // res는 초기값 (res * 10 + 1) * 10 + 1....
+		while (true) {
+			if (cnt % n == 0) {
+				printf("%d\n", res);
+				break;
+			}
+			else {
+				cnt = (cnt * 10) + 1;
+				cnt %= n; // 모듈러 연산..
+				res++;
 			}
 		}
-		return 0;
 	}
+	
+	return 0;
 }
