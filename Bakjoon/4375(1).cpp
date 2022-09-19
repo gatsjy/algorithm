e1c042b82c7e5a753b4b370a1befd9170694c9ed
Gatsjy <gkswndks123@naver.com>
2022-09-19 23:58:46 +0900

    2022-09-19, 한주안, dfs시작

--- Bakjoon/4375(1).cpp
+++ Bakjoon/4375(1).cpp
@@ -2,23 +2,24 @@
 
 using namespace std;
 
-int n;
-int main() {
+namespace _4375 {
+	int n;
+	int main() {
 
-	while (scanf("%d",&n) != EOF) {
-		int cnt = 1, res = 1; // res는 초기값 (res * 10 + 1) * 10 + 1....
-		while (true) {
-			if (cnt % n == 0) {
-				printf("%d\n", res);
-				break;
-			}
-			else {
-				cnt = (cnt * 10) + 1;
-				cnt %= n; // 모듈러 연산..
-				res++;
+		while (scanf("%d", &n) != EOF) {
+			int cnt = 1, res = 1; // res는 초기값 (res * 10 + 1) * 10 + 1....
+			while (true) {
+				if (cnt % n == 0) {
+					printf("%d\n", res);
+					break;
+				}
+				else {
+					cnt = (cnt * 10) + 1;
+					cnt %= n; // 모듈러 연산..
+					res++;
+				}
 			}
 		}
+		return 0;
 	}
-	
-	return 0;
 }
