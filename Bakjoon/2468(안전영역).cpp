55f3121ab2c3fbc944a58df84678cf3bd59f59f3
Gatsjy <gkswndks123@naver.com>
2022-09-22 20:26:16 +0900

    2022-09-22, 한주안, 안전영역

--- Bakjoon/2468(안전영역).cpp
+++ Bakjoon/2468(안전영역).cpp
@@ -4,63 +4,50 @@ using namespace std;
 
 int n; // n은 2이상 100이하의 정수
 int visited[101][101], a[101][101];
-int max_height = -2147000000;
-int input;
-int max_cnt = 0; // 물에 잠기지 않는 최대 갯수
-int ret = 0;
-int res = 0;
+int ret = 1;
 int x, y;
 int dy[4] = {-1,0,1,0};
 int dx[4] = { 0,1,0,-1 };
 
-void dfs(int y, int x) {
+void dfs(int y, int x, int d) {
 	visited[y][x] = 1;
 	for (int i = 0; i < 4; i++) {
 		int ny = dy[i] + y;
 		int nx = dx[i] + x;
-		if (ny < 0 || nx < 0 || ny >= n || nx >= n)continue;
-		if (!visited[ny][nx]) {
-			dfs(ny, nx);
+		if (ny < 0 || nx < 0 || ny >= n || nx >= n) continue;
+		if (!visited[ny][nx] && a[ny][nx] > d) {
+			dfs(ny, nx, d);
 		}
 	}
 	return;
 }
 int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
 
 	cin >> n;
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			cin >> input;
-			a[i][j] = input;
-			max_height = max(max_height, input);
+			cin >> a[i][j];
 		}
 	}
 
-	for (int i = 1; i < max_height; i++) {
-		ret = 0;
+	for (int d = 1; d < 101; d++) {
 		fill(&visited[0][0], &visited[0][0] + 101 * 101, 0);
-		for (int j = 0; j < n; j++) {
-			for (int k = 0; k < n; k++) {
-				if (i >= a[j][k]) visited[j][k] = 1;
-			}
-		}
-
+		int cnt = 0;
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
-				if (visited[i][j]) {
-					dfs(i, j);
-					ret++;
+				if (a[i][j] > d && !visited[i][j]) {
+					dfs(i, j, d);
+					cnt++;
 				}
 			}
 		}
-		if (ret > max_cnt) {
-			max_cnt = ret;
-			res = i;
-		}
+		ret = max(ret, cnt);
 	}
 
-	cout << res;
+	cout << ret << "\n";
 
 
 	return 0;
