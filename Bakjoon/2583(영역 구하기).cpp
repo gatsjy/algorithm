fedacc6d3f431e159bc4f75be929df3b80ba99ef
Gatsjy <gkswndks123@naver.com>
2022-09-23 23:51:21 +0900

    2022-09-23, 한주안, 뭔가.. 로직을 다시 짜야할 듯 ㅠㅠ x,y 개념부터 바로잡자 ㅠㅠㅠㅠ

--- Bakjoon/2583(영역 구하기).cpp
+++ Bakjoon/2583(영역 구하기).cpp
@@ -4,10 +4,26 @@ using namespace std;
 
 int m, n, k;
 int d;
-
+int cnt;
 int a[101][101], visited[101][101];
+vector<int> res;
 
 int yy2, yy1, xx2, xx1;
+int dy[4] = { -1,0,1,0 };
+int dx[4] = { 0,1,0,-1 };
+void dfs(int y, int x) {
+	visited[y][x] = 1;
+	cnt += 1;
+	for (int i = 0; i < 4; i++) {
+		int ny = dy[i] + y;
+		int nx = dx[i] + x;
+		if (ny < 0 || nx < 0 || ny >= m || nx >= n) continue;
+		if (!visited[ny][nx]) {
+			dfs(ny, nx);
+		}
+	}
+	return;
+}
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -16,18 +32,34 @@ int main() {
 
 	cin >> m >> n >> k;
 
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			a[i][j] = 1;
+		}
+	}
+
 	for (int d = 0; d < k; d++) {
 		// y1, x1 -> 왼쪽아래
 		// y2, x2 -> 오른쪽위
-		cin >> yy1 >> xx1 >> yy2 >> xx2;
+		//cin >> yy1 >> xx1 >> yy2 >> xx2;
+		cin >> xx1 >> yy1 >> xx2 >> yy2;
 
-		for (int i = 0; i < (yy2 - yy1); i++) {
-			for (int j = 0; j < (xx2 - xx1); j++) {
-				visited[i][j] = 1;
+		for (int i = yy1; i < yy2; i++) {
+			for (int j = xx1; j < xx2; j++) {
+				visited[j][i] = 1;
 			}
 		}
 	}
 
-	cout << "1" << endl;
+	// 근데 상관 없잖어..?
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			if (!visited[i][j]) {
+				cnt = 0;
+				dfs(i, j);
+				res.push_back(cnt);
+			}
+		}
+	}
 	return 0;
 }
