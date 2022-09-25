cf5522c2bba105db010cc72146d9b414011c0703
Gatsjy <gkswndks123@naver.com>
2022-09-25 19:58:57 +0900

    2022-09-25, 한주안, 피보나치수4

--- Bakjoon/2748(피보나치수2).cpp
+++ Bakjoon/2748(피보나치수2).cpp
@@ -4,19 +4,21 @@ using namespace std;
 
 typedef long long ll;
 
-ll a[101];
+ll a[10001];
+
+ll mod = 1000000; 
+int fibo(int n) {
+	if (n == 1 || n == 0) return n;
+	return fibo(n - 2) % mod + fibo(n - 1) % mod;
+}
 
 int main() {
 
-	a[0] = 0;
 	a[1] = 1;
 
 	int n;
 	cin >> n;
-	for (int i = 2; i <= n; i++) {
-		a[i] = a[i - 2] + a[i - 1];
-	}
 
-	cout << a[n];
+	cout << fibo(n);
 	return 0;
 }
