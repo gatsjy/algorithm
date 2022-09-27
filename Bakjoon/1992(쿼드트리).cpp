6af94aa5cf23b2babcfde96c4f42c1db9076f42b
Gatsjy <gkswndks123@naver.com>
2022-09-27 22:41:44 +0900

    2022-09-27, 한주안, 쿼드트리.. 하지만 답은 아님. .ㅠ

--- Bakjoon/1992(쿼드트리).cpp
+++ Bakjoon/1992(쿼드트리).cpp
@@ -6,20 +6,26 @@ int n;
 int a[81][81];
 
 string go(int y, int x, int cnt) {
-	return "";
+	int nct = cnt / 2;
+	if (cnt == 1) return to_string(a[y][x]);
+	string go1 = go(y, x, nct);
+	string go2 = go(y, x + nct, nct);
+	string go3 = go(y + nct, x, nct);
+	string go4 = go(y + nct, x + nct, nct);
+	if (go1 == go2 && go2 == go3 && go3 == go4) return go1;
+	return "(" + go1 + go2 + go3 + go4 + ")";
 }
 int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
 	cin >> n;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			scanf_s("%1d", &a[i][j]);
+			scanf("%1d", &a[i][j]);
 		}
 	}
 
 	// 4번의 쿼드트리 로직을 발생시킨다.
-	go(0, 0, n / 2); // (0,0) + 4
-	go(0 + n / 2, 0, n / 2); // (4, 0) + 4;
-	go(0, 0 + n / 2, n / 2); // (0, 4) + 4;
-	go(n / 2, n / 2, n / 2); // (4, 4) + 4;
+	cout << go(0, 0, n);
 	return 0;
 }
