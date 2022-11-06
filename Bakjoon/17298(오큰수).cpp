4c5ecb2b707f74471cafcda580be56a96d0109f0
Gatsjy <gkswndks123@naver.com>
2022-11-06 09:24:07 +0900

    2022-11-06, 한주안, 트리

--- Bakjoon/17298(오큰수).cpp
+++ Bakjoon/17298(오큰수).cpp
@@ -1,25 +1,25 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-int a[1000004];
-int ret[1000004];
-int n;
-stack<int> st;
-int main() {
-	cin >> n;
-	memset(ret, -1, sizeof(ret));
-	for (int i = 0; i < n; i++) {
-		cin >> a[i];
-		while (st.size() && a[st.top()] < a[i]) {
-			ret[st.top()] = a[i];
-			st.pop();
-		}
-		st.push(i);
-	}
-	for (int i = 0; i < n; i++) {
-		cout << ret[i] << " ";
-	}
-
-	return 0;
-}
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+//int a[1000004];
+//int ret[1000004];
+//int n;
+//stack<int> st;
+//int main() {
+//	cin >> n;
+//	memset(ret, -1, sizeof(ret));
+//	for (int i = 0; i < n; i++) {
+//		cin >> a[i];
+//		while (st.size() && a[st.top()] < a[i]) {
+//			ret[st.top()] = a[i];
+//			st.pop();
+//		}
+//		st.push(i);
+//	}
+//	for (int i = 0; i < n; i++) {
+//		cout << ret[i] << " ";
+//	}
+//
+//	return 0;
+//}
