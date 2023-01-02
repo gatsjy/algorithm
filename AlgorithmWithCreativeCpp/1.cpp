41248efffc492662b9b2cd67af5dad88686c11f5
Gatsjy <gkswndks123@naver.com>
2023-01-02 22:33:30 +0900

    2023-01-02, 2번째 문제

--- AlgorithmWithCreativeCpp/1.cpp
+++ AlgorithmWithCreativeCpp/1.cpp
@@ -1,15 +1,15 @@
-#include<bits/stdc++.h>
-
-using namespace std;
-int n, m, sum;
-int main() {
-
-	cin >> n >> m;
-	for (int i = 1; i <= n; i++) {
-		if (!(i % m)) {
-			sum += i;
-		}
-	}
-	cout << sum;
-	return 0;
-}
+//#include<bits/stdc++.h>
+//
+//using namespace std;
+//int n, m, sum;
+//int main() {
+//
+//	cin >> n >> m;
+//	for (int i = 1; i <= n; i++) {
+//		if (!(i % m)) {
+//			sum += i;
+//		}
+//	}
+//	cout << sum;
+//	return 0;
+//}
---
+++ AlgorithmWithCreativeCpp/2.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n, m,sum;
+int main() {
+	cin >> n >> m;
+	cout << n;
+	sum += n;
+	for (int i = n + 1; i <= m; i++) {
+		cout << " + ";
+		cout << i;
+		sum += i;
+	}
+	cout << " = ";
+	cout << sum;
+	return 0;
+}
