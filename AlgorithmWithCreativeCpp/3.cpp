dd4eb8ee3c079ecca00f336e3e7447249d438be5
Gatsjy <gkswndks123@naver.com>
2023-01-03 19:55:59 +0900

    2023-01-03, 3일차

---
+++ AlgorithmWithCreativeCpp/3.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n, m, sum;
+int main() {
+	cin >> n;
+	cout << 1;
+	sum += 1;
+	for (int i = 2; i <= n/2; i++) {
+		if (!(n % i)) {
+			cout << " + ";
+			cout << i;
+			sum += i;
+		}
+	}
+	cout << " = ";
+	cout << sum;
+	return 0;
+}
