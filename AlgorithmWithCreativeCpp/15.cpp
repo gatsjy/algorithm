ad4bc9a1df11282efe7c4e88e47328f8adf75638
hja0691@hanmail.net <hja0691@hanmail.net>
2023-01-15 11:15:53 +0900

    2023-01-15, 15

---
+++ AlgorithmWithCreativeCpp/15.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int n,cnt;
+int ch[200001];
+int main() {
+	cin >> n;
+	for (int i = 2; i <= n; i++) {
+		if (ch[i]) continue;
+		for (int k = 2; k * i <= n; k++) {
+			if (!ch[k * i]) ch[k * i] = 1;
+		}
+	}
+
+	for (int i = 1; i <= n; i++) {
+		if (!ch[i])cnt++;
+	}
+	return 0;
+}
