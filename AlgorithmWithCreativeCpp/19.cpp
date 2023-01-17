885cb134a382357cd7e4961270c01cd457bd373f
Gatsjy <gkswndks123@naver.com>
2023-01-17 20:31:40 +0900

    2023-01-17, 17

---
+++ AlgorithmWithCreativeCpp/19.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+int h[101];
+
+int main() {
+	cin >> n;
+	// °Å²Ù·Î
+	for (int i = 0; i < n; i++) {
+		cin >> h[i];
+	}
+	
+	int cnt = 0;
+	int mx = h[n-1];
+	for (int i = n - 1; i > 0; i--) {
+		if (h[i] > mx) {
+			cnt++;
+			mx = h[i];
+		}
+	}
+	cout << cnt;
+
+	return 0;
+}
