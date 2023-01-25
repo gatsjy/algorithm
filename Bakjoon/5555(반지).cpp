b3c16928d4a1e54fa819a203a3cf8066d95b1d7e
Gatsjy <gkswndks123@naver.com>
2023-01-26 00:23:58 +0900

    2023-01-26, 26

---
+++ Bakjoon/5555(반지).cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string s, tmp;
+int c, n, cnt;
+int main() {
+	cin >> s;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> tmp;
+		string bangi = tmp + tmp;
+		if (bangi.find(s) != string::npos) {
+			cnt++;
+		}
+	}
+	cout << cnt;
+	return 0;
+}
