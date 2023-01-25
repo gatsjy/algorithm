822d203647a88efdc230437161a19beff9f125e0
Gatsjy <gkswndks123@naver.com>
2023-01-25 20:43:52 +0900

    2023-01-25, 25

---
+++ Bakjoon/3058(짝수를 찾아라).cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int c,n;
+int main() {
+	cin >> c;
+	while (c--) {
+		vector<int> v;
+		for (int i = 0; i < 7; i++) {
+			cin >> n;
+			v.push_back(n);
+		}
+		sort(v.begin(), v.end(), greater<>());
+		int res = 0;
+		int sum = 0;
+		for (int i = 0; i < 7; i++) {
+			if (!(v[i] % 2)) {
+				sum += v[i];
+				res = v[i];
+			}
+		}
+		cout << sum << " " << res << "\n";
+	}
+
+	return 0;
+}
--- Bakjoon/5176(대회자리).cpp
+++ Bakjoon/5176(대회자리).cpp
@@ -1,30 +1,30 @@
 #include <bits/stdc++.h>
-
-using namespace std;
-
-int k, p,m,n;
-int ch[501];
-int main() {
-	cin >> k;
-	
-	while (k--) {
-		int res = 0;
-		fill(ch, ch + 501, 0);
-		cin >> p >> m;
-		for (int i = 1; i <= m; i++) {
-			ch[i] = 1;
-		}
-		for (int i = 0; i < p; i++) {
-			cin >> n;
-			if (ch[n] == 1) {
-				ch[n]++;
-			}
-			else {
-				res++;
-			}
-		}
-		cout << res << "\n";
-	}
-
-	return 0;
-}
+//
+//using namespace std;
+//
+//int k, p,m,n;
+//int ch[501];
+//int main() {
+//	cin >> k;
+//	
+//	while (k--) {
+//		int res = 0;
+//		fill(ch, ch + 501, 0);
+//		cin >> p >> m;
+//		for (int i = 1; i <= m; i++) {
+//			ch[i] = 1;
+//		}
+//		for (int i = 0; i < p; i++) {
+//			cin >> n;
+//			if (ch[n] == 1) {
+//				ch[n]++;
+//			}
+//			else {
+//				res++;
+//			}
+//		}
+//		cout << res << "\n";
+//	}
+//
+//	return 0;
+//}
