3cf38769833d4dae993264b7ee2dbcb71d0750d6
Gatsjy <gkswndks123@naver.com>
2023-01-28 17:20:56 +0900

    2023-01-28, 11008

--- Bakjoon/11944(NN).cpp
+++ Bakjoon/11944(NN).cpp
@@ -1,17 +1,17 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-string s;
-int n,m;
-int main() {
-	cin >> n >> m;
-
-	string tmp = "";
-	for (int i = 0; i < n; i++) {
-		tmp += to_string(n);
-	}
-
-	cout << tmp.substr(0, m);
-	return 0;
-}
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+//string s;
+//int n,m;
+//int main() {
+//	cin >> n >> m;
+//
+//	string tmp = "";
+//	for (int i = 0; i < n; i++) {
+//		tmp += to_string(n);
+//	}
+//
+//	cout << tmp.substr(0, m);
+//	return 0;
+//}
