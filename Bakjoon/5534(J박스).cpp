70c5e218cab956e7787795a3f6cfb9408ff98e24
Gatsjy <gkswndks123@naver.com>
2023-01-31 18:46:48 +0900

    2023-01-31, 31

---
+++ Bakjoon/5534(J박스).cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int c, n;
+int main() {
+	cin >> c;
+	while (c--) {
+		cin >> n;
+		if (n == 1) {
+			cout << "#" << "\n";
+			continue;
+		}
+
+		for (int i = 0; i < n; i++) {
+			cout << "#";
+		}
+		cout << "\n";
+		
+		for (int i = 0; i < n - 2; i++) {
+			cout << "#";
+			for (int j = 0; j < n - 2; j++) {
+				cout << "J";
+			}
+			cout << "#" << "\n";
+		}
+
+		for (int i = 0; i < n; i++) {
+			cout << "#";
+		}
+
+		cout << "\n";
+		cout << "\n";
+	}
+	return 0;
+}
