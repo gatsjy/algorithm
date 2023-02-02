570272677e0509281c0142149b8b2557fd1d3fa3
Gatsjy <gkswndks123@naver.com>
2023-02-02 23:25:09 +0900

    2023-02-02,

---
+++ Bakjoon/12778(CTP공국).cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int t,m, tmpi;
+string s, tmps;
+
+int main() {
+	cin >> t;
+	while (t--) {
+		cin >> m;
+		cin >> s;
+		if (s == "C") {
+			for (int i = 0; i < m; i++) {
+				cin >> tmps;
+				cout << tmps[0] - 'A'+1 << " ";
+			}
+		}
+		else {
+			for (int i = 0; i < m; i++) {
+				cin >> tmpi;
+				cout << (char)(tmpi+'A'-1) << " ";
+			}
+		}
+		cout << "\n";
+	}
+	return 0;
+}
