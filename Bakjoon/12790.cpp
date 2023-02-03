7319040fccd8ad941699a80959bf55383bc19ded
Gatsjy <gkswndks123@naver.com>
2023-02-03 22:23:45 +0900

    2023-02-03

---
+++ Bakjoon/12790.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int t;
+int a, b, c, d, e, f, g, h, i, j;
+int main() {
+	cin >> t;
+	while (t--) {
+		cin >> a >> b >> c >> d >> e >> f >> g >> h;
+		if (a < 1 ) {
+			a = 1;
+		}
+		if (b < 1) {
+			b = 1;
+		}
+		if (e < 1) {
+			e = 1;
+		}
+		if (f < 1) {
+			f = 1;
+		}
+		if (c < 0) {
+			c = 0;
+		}
+		if (g < 0) {
+			g = 0;
+		}
+		cout << a + e + b * 5 + f * 5 + c * 2 + g*2 + d * 2 + h*2 << "\n";
+	}
+	return 0;
+}
